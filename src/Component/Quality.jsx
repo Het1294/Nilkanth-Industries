cf63a56040f635e5f925f07a0817314c9b945865
Het Patel <159883443+Het1294@users.noreply.github.com>
2025-02-20 10:34:12 +0530

    second

--- src/Component/Quality.jsx
+++ src/Component/Quality.jsx
@@ -7,9 +7,9 @@ const Quality = () => {
     return (
         <div className='quality-page'>
             <div className='quality-cont'>
-                <div className='quality-head'>
-
+                <div>
                 <h1>Quality Policy</h1>
+
 
                 <p>Nilkanth Engineering's focus is to build long-term, successful relationships with customers. This will be achieved through a process of continuous improvement, understanding our customer’s expectations, providing innovative solutions, engineering support and quality machines in a timely fashion.
                     </p><p>  Our products are 100% safe to use and are designed to make lives better. Leading companies are among the satisfied customers of the services offered by us. Stay assured of the quality of our amazing products. Many payment options are available, and with emergency delivery options, it becomes a complete package of trust and genuineness.</p>
